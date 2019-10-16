12a15d250336f336d1b6408661227714d933260e
kaveri sonani <zinisonani11@gmail.com>
2019-10-16 15:42:52 -0400

    Controller Method for List and View

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private EFContext context = new EFContext();
+
+        public ActionResult List()
+        {
+            var customers = context.Customers.OrderBy(o => o.FirstName).ThenBy(p => p.LastName).ToList();
+            return View(customers);
+        }
+
+
+            
+    }
+}
