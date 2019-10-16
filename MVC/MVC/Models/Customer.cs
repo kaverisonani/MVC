869912a9eb013312eb4995df59af07289072ee1a
kaveri sonani <zinisonani11@gmail.com>
2019-10-15 21:08:44 -0400

    Add Model Class

---
+++ MVC/MVC/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 CustomerID { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "First Name Required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "Last Name Required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Birth Date Required")]
+        [Display(Name = "Birth Date")]
+        public Nullable<DateTime> BirthDate { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(320)]
+        [Required(ErrorMessage = "Email Required")]
+        //[RegularExpression(@"^(0[1-9]|[12][0-9]|3[01])[-/.](0[1-9]|1[012])[-/.](19|20)\d\d$", ErrorMessage = "not a valid format")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "Address Required")]
+        public string Address { get; set; }
+    }
+}
---
+++ MVC/MVC/Models/EFContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class EFContext : DbContext
+    {        
+            public EFContext()
+                : base("name=cnStr")
+            {
+
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                //Example of using FluentAPI
+                //modelBuilder.Entity<Customer>()
+                //     .HasMany<Customer>(c => c.Customer)
+                //     .WithRequired(cus => cus.Country)
+                //     .HasForeignKey(cus => cus.CountryID)
+                //     .WillCascadeOnDelete(false);
+
+            }
+
+            public virtual DbSet<Customer> Customers { get; set; }
+        }
+
+    }
