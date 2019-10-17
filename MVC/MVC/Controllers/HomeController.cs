8ad3fbd0de61a068227dde21472dd50631f232c1
kaveri sonani <zinisonani11@gmail.com>
2019-10-17 14:29:55 -0400

    Insert and Update

--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,7 +19,61 @@ namespace MVC.Controllers
             return View(customers);
         }
 
+        public ActionResult AddUpdate(Int32? id)
+        {
+            if (id == null)
+            {
+                ViewBag.TiTle = "Add";
+                var customer = new Customer();
+                return View("~/Views/Home/Save.cshtml", customer);
+            }
+            else
+            {
+                ViewBag.TiTle = "Update";
+                Int64 customerID = Convert.ToInt64(id);
+                var customers = context.Customers.Where(t => t.CustomerID == customerID).OrderBy(o => o.FirstName).ThenBy(p => p.LastName).SingleOrDefault();
+                return View("~/Views/Home/Save.cshtml", customers);
+            }
+        }
 
-            
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Customer obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Convert.ToInt64(obj.CustomerID) > 0)
+                {
+                    context.Entry(obj).State = EntityState.Modified;      //To Update all fields
+
+                    ////To Update Induvidual fields
+                    //context.Customers.Attach(obj);
+                    //context.Entry(obj).Property(u => u.FirstName).IsModified = true;
+                    //context.Entry(obj).Property(u => u.LastName).IsModified = true;
+                }
+                else
+                {
+                    context.Customers.Add(obj);
+                }
+
+                if (context.SaveChanges() > 0)
+                {
+                    TempData["Message"] = "Saved Successfully";
+                }
+                else
+                {
+                    TempData["Message"] = "Error whle saving";
+                }
+                return RedirectToAction("List", "Home");
+            }
+
+            return View("~/Views/Home/Save.cshtml", obj);
+        }
+
+
+
+
+
+
     }
 }
